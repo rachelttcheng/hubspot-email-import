7115892b8511dc767947312011e237ef05bc6c49
Rachel Cheng <rtcheng@umich.edu>
2024-03-08 15:48:36 -0500

    added more consistent rate limiting for contacts api calls, also added precautions for not attempting to duplicate preexisting contact and throwing a conflict error

--- contacts-api.py
+++ contacts-api.py
@@ -7,55 +7,31 @@ import hubspot
 import csv
 import requests
 import json
-import time
+from ratelimiter import checkLimit
 from pprint import pprint
 from hubspot.crm.contacts import BatchInputSimplePublicObjectInputForCreate, ApiException
 from get_token import fetchToken
 
 ACCESS_TOKEN = fetchToken()
-RATE_LIMIT_PER_SEC = 10
+COMPANY_SEARCH_URL = "https://api.hubapi.com/crm/v3/objects/companies/search"
+CONTACTS_SEARCH_URL = "https://api.hubapi.com/crm/v3/objects/contacts/search"
 
 client = hubspot.Client.create(access_token=ACCESS_TOKEN)
 
-COMPANY_SEARCH_URL = "https://api.hubapi.com/crm/v3/objects/companies/search"
-
-# rate limiter for querying api for match for company domain
-class rateLimiter():
-    def __init__(self, rate_limit_per_second):
-        self.rate_limit_per_second = rate_limit_per_second
-        self.last_called = 0
-    
-    def callAPI(self, companyDomain):
-        # space out calls, based on time since last call
-        now = time.time()
-        time_since_last_call = now - self.last_called
-        if (time_since_last_call < (1 / self.rate_limit_per_second)):
-            time.sleep(1 / (self.rate_limit_per_second - time_since_last_call))
-        
-        # return response data from making request
-        return self.make_api_request(companyDomain)
-
-    def make_api_request(self, company_domain):
-        # insert company domain into query and create relevant headers
-        payload = json.dumps({"query": company_domain})
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + ACCESS_TOKEN
-        }
-
-        # make request
-        response = requests.request("POST", COMPANY_SEARCH_URL, headers=headers, data=payload)
-        responseData = response.json()
-
-        # return data
-        return responseData
-
-caller = rateLimiter(rate_limit_per_second=10)
-
-# given a company domain (unique identifier), makes request to hubspot api and returns object id within hubspot databse
+# given a company domain (unique identifier), makes request to hubspot api and returns object id within hubspot database
 def getAssociatedCompanyID(companyDomain):
-    # get response data, with rate limiting
-    responseData = caller.callAPI(companyDomain)
+    # insert company domain into query and create relevant headers
+    payload = json.dumps({"query": companyDomain})
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + ACCESS_TOKEN
+    }
+
+    # make request and get response data, with rate limiting
+    checkLimit()
+    response = requests.request("POST", COMPANY_SEARCH_URL, headers=headers, data=payload)
+    responseData = response.json()
+
     print(responseData)
 
     # handle if no result; still TO DO in regards to if program should terminate or not
@@ -64,6 +40,28 @@ def getAssociatedCompanyID(companyDomain):
 
     # return company id
     return responseData["results"][0]["id"]
+
+# given a contact email, makes request to hubspot api and ensures no duplication of existing contact (prevent conflict erros)
+def contactAlreadyExists(contactEmail):
+    # insert company domain into query and create relevant headers
+    payload = json.dumps({"query": contactEmail})
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + ACCESS_TOKEN
+    }
+
+    # make request and get response data, with rate limiting
+    checkLimit()
+    response = requests.request("POST", CONTACTS_SEARCH_URL, headers=headers, data=payload)
+    responseData = response.json()
+
+    print(f"contact {contactEmail} has {responseData['total']} matches")
+
+    # check if any matches exist and return accordingly
+    if responseData["total"] == 0:
+        return False
+    else:
+        return True
 
 def main():
     # flow csv data into nested json format
@@ -88,7 +86,7 @@ def main():
                     "website": row["company domain"]
                 }
             }
-            for row in csv.DictReader(contactsFile)
+            for row in csv.DictReader(contactsFile) if not contactAlreadyExists(row["email"])
         ]
 
     batch_input_simple_public_object_input_for_create = BatchInputSimplePublicObjectInputForCreate(inputs=contacts)
@@ -97,6 +95,7 @@ def main():
         api_response = client.crm.contacts.batch_api.create(batch_input_simple_public_object_input_for_create=batch_input_simple_public_object_input_for_create)
         pprint(api_response)
     except ApiException as e:
+
         print("Exception when calling batch_api->create: %s\n" % e)
 
 if __name__ == "__main__":
