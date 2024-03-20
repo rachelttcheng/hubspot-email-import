f16e184f35cdb9d233d61a2f8bb469a865b70090
Rachel Cheng <rtcheng@umich.edu>
2024-03-20 15:29:51 -0700

    updated companies api call to adhere to limit of 100 companies per batch request

--- companies_api.py
+++ companies_api.py
@@ -1,4 +1,4 @@
-# create/update companies before contacts
+# create/update companies before contacts, in batches of 100 (size limit)
 # url = 'https://api.hubapi.com/crm/v3/objects/companies/batch/create'
 
 # run on command line as such:
@@ -42,22 +42,42 @@ def companyExists(companyDomain):
 
 def callCompaniesAPI(contactsFilename):
     # flow company info into json format
-    companies = []
     with open(contactsFilename, newline='') as contactsFile:
-        companies = [
-            {
-                "properties": {
-                    "domain": row["company domain"]
-                }
-            }
-            for row in csv.DictReader(contactsFile) if not companyExists(row["company domain"])
-        ]
+        # variables to keep track of batches
+        companies = set()
+        current_batch_size = 0
+        companies_batch = list()
 
-    batch_input_simple_public_object_input_for_create = BatchInputSimplePublicObjectInputForCreate(inputs=companies)
+        # iterate through csv file and add companies that don't already exist, either within local companies list or hubspot databse into batch request
+        for row in csv.DictReader(contactsFile):
+            # check if company already exists within larger file set, then check if it already exists within hubspot database
+            if row["company domain"] not in companies and not companyExists(row["company domain"]):
+                companies.add(row["company domain"])
+                companies_batch.append({"properties": {"domain": row["company domain"]}})
+                current_batch_size += 1
 
-    try:
-        api_response = client.crm.companies.batch_api.create(batch_input_simple_public_object_input_for_create=batch_input_simple_public_object_input_for_create)
-        pprint(api_response)
-        print("\nCompanies created successfully.\n")
-    except ApiException as e:
-        print("Exception when calling batch_api->create: %s\n" % e)
+                # push batch if size is at limit of 100
+                if current_batch_size == 100:
+                    batch_input_simple_public_object_input_for_create = BatchInputSimplePublicObjectInputForCreate(inputs=companies_batch)
+
+                    try:
+                        api_response = client.crm.companies.batch_api.create(batch_input_simple_public_object_input_for_create=batch_input_simple_public_object_input_for_create)
+                        pprint(api_response)
+                        print("\nCompany batch pushed successfully.\n")
+                    except ApiException as e:
+                        print("Exception when calling batch_api->create: %s\n" % e)
+
+                    # reset batch
+                    current_batch_size = 0
+                    companies_batch = list()
+
+        # make sure to push leftover/last batch if it didn't hit batch limit of 100
+        if current_batch_size > 0:
+            batch_input_simple_public_object_input_for_create = BatchInputSimplePublicObjectInputForCreate(inputs=companies_batch)
+
+            try:
+                api_response = client.crm.companies.batch_api.create(batch_input_simple_public_object_input_for_create=batch_input_simple_public_object_input_for_create)
+                pprint(api_response)
+                print("\nCompany batch pushed successfully.\n")
+            except ApiException as e:
+                print("Exception when calling batch_api->create: %s\n" % e)
