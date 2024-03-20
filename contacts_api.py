b9971392895f7e1a169ebfcaa4dbd9f2a9b7b512
Rachel Cheng <rtcheng@umich.edu>
2024-03-20 16:07:04 -0700

    added batch limitation of size 100 to adhere to hubspot api reqs; also added more detailed program finish response

--- contacts_api.py
+++ contacts_api.py
@@ -1,4 +1,4 @@
-# import contacts (after companies)
+# import contacts (after companies), limited to batch sizes of 100
 # all contacts need to be in JSON format, associated to company object
 
 # run on command line as such:
@@ -35,8 +35,6 @@ def getAssociatedCompanyID(companyDomain):
     response = requests.request("POST", COMPANY_SEARCH_URL, headers=headers, data=payload)
     responseData = response.json()
 
-    print(responseData)
-
     # handle if no result; still TO DO in regards to if program should terminate or not
     if responseData["total"] == 0:
         print(f"Company {companyDomain} does not exist, cannot associate to contact\n")
@@ -44,7 +42,7 @@ def getAssociatedCompanyID(companyDomain):
     # return company id
     return responseData["results"][0]["id"]
 
-# given a contact email, makes request to hubspot api and ensures no duplication of existing contact (prevent conflict erros)
+# given a contact email, makes request to hubspot api and ensures no duplication of existing contact (prevent conflict errors)
 def contactAlreadyExists(contactEmail):
     # insert company domain into query and create relevant headers
     payload = json.dumps({"query": contactEmail})
@@ -66,35 +64,61 @@ def contactAlreadyExists(contactEmail):
 
 def callContactsAPI(contactsFilename):
     # flow csv data into nested json format
-    contacts = []
     with open(contactsFilename, newline='') as contactsFile:
-        contacts = [
-            {
-                "associations": [{
-                    "types": [{
-                        "associationCategory": "HUBSPOT_DEFINED",
-                        "associationTypeId": 1
+        # variables to keep track of locally seen contacts, and current batch
+        contacts = set()
+        contacts_batch = list()
+        total_contacts_pushed = 0
+
+        # iterate through contacts file and add unique contacts to batch request
+        for row in csv.DictReader(contactsFile):
+            # check if contact already exists within larger file set, then check if it already exists within hubspot database
+            if row["email"] not in contacts and not contactAlreadyExists(row["email"]):
+                contacts.add(row["email"])
+                contacts_batch.append({
+                    "associations": [{
+                        "types": [{
+                            "associationCategory": "HUBSPOT_DEFINED",
+                            "associationTypeId": 1
+                        }],
+                        "to": {
+                            "id": getAssociatedCompanyID(row["company domain"])
+                        }
                     }],
-                    "to": {
-                        "id": getAssociatedCompanyID(row["company domain"])
+                    "properties": {
+                        "email": row["email"],
+                        "firstname": row["first name"],
+                        "lastname": row["last name"],
+                        "company": row["company"],
+                        "website": row["company domain"]
                     }
-                }],
-                "properties": {
-                    "email": row["email"],
-                    "firstname": row["first name"],
-                    "lastname": row["last name"],
-                    "company": row["company"],
-                    "website": row["company domain"]
-                }
-            }
-            for row in csv.DictReader(contactsFile) if not contactAlreadyExists(row["email"])
-        ]
+                })
+            
+                # push batch if at batch size limit of 100
+                if len(contacts_batch) == 100:
+                    batch_input_simple_public_object_input_for_create = BatchInputSimplePublicObjectInputForCreate(inputs=contacts_batch)
 
-    batch_input_simple_public_object_input_for_create = BatchInputSimplePublicObjectInputForCreate(inputs=contacts)
+                    try:
+                        api_response = client.crm.contacts.batch_api.create(batch_input_simple_public_object_input_for_create=batch_input_simple_public_object_input_for_create)
+                        total_contacts_pushed += len(api_response.results)
+                        # pprint(api_response) # -- UNCOMMENT IF WANT MORE DETAILED RESPONSE INFO
+                        print("\nContacts batch pushed successfully.\n")
+                    except ApiException as e:
+                        print("Exception when calling batch_api->create: %s\n" % e)
 
-    try:
-        api_response = client.crm.contacts.batch_api.create(batch_input_simple_public_object_input_for_create=batch_input_simple_public_object_input_for_create)
-        pprint(api_response)
-        print("\nContacts created successfully.\n")
-    except ApiException as e:
-        print("Exception when calling batch_api->create: %s\n" % e)
+                    # reset batch
+                    contacts_batch = list()
+        
+        # make sure to push leftover/last batch if it didn't hit batch limit of 100
+        if len(contacts_batch) > 0:
+            batch_input_simple_public_object_input_for_create = BatchInputSimplePublicObjectInputForCreate(inputs=contacts_batch)
+
+            try:
+                api_response = client.crm.contacts.batch_api.create(batch_input_simple_public_object_input_for_create=batch_input_simple_public_object_input_for_create)
+                total_contacts_pushed += len(api_response.results)
+                # pprint(api_response) # -- UNCOMMENT IF WANT MORE DETAILED RESPONSE INFO
+                print("\nContacts batch pushed successfully.\n")
+            except ApiException as e:
+                print("Exception when calling batch_api->create: %s\n" % e)
+
+        print(f"\n{total_contacts_pushed} contacts pushed successfully.\n")
